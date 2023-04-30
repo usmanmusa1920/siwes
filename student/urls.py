9f10fa7d9e814f7422c51e4e707ac7d79694e5ec
Usman Musa <usmanmusa1920@gmail.com>
2023-04-30 21:36:59 +0100

    make student update acceptance letter functionalities

--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import (studentProfile, otherViewStudentProfile, placementLetter, acceptanceLetter,
-  uploadedAcceptanceLetter, uploadAcceptanceLetter200, uploadAcceptanceLetter300, requestChangeAcceptanceLetter, updateAcceptanceLetterRequest200, updateAcceptanceLetterRequest300)
+  uploadedAcceptanceLetter200, uploadedAcceptanceLetter300, uploadAcceptanceLetter200, uploadAcceptanceLetter300, updateAcceptanceLetter200, updateAcceptanceLetter300, updateAcceptanceLetterRequest200, updateAcceptanceLetterRequest300,
+  requestChangeAcceptanceLetter)
 
 
 app_name = "student"
@@ -10,10 +11,16 @@ urlpatterns = [
   path("student/profile/<int:student_id>", otherViewStudentProfile, name="other_view_student_profile"),
   path("student/placement/letter", placementLetter, name="placement_letter"),
   path("student/acceptance/letter", acceptanceLetter, name="acceptance_letter"),
-  path("student/upload/acceptance/letter", uploadedAcceptanceLetter, name="uploaded_acceptance_letter"),
+  path("student/uploaded/acceptance/letter/200", uploadedAcceptanceLetter200, name="uploaded_acceptance_letter_200"),
+  path("student/uploaded/acceptance/letter/300", uploadedAcceptanceLetter300, name="uploaded_acceptance_letter_300"),
+
   path("student/upload/acceptance/letter/200", uploadAcceptanceLetter200, name="upload_acceptance_letter_200"),
   path("student/upload/acceptance/letter/300", uploadAcceptanceLetter300, name="upload_acceptance_letter_300"),
+
+  path("student/update/acceptance/letter/200", updateAcceptanceLetter200, name="update_acceptance_letter_200"),
+  path("student/update/acceptance/letter/300", updateAcceptanceLetter300, name="update_acceptance_letter_300"),
+
+  path("request/update/acceptance/letter/<int:letter_id>/200", updateAcceptanceLetterRequest200, name="update_acceptance_letter_request_200"),
+  path("request/update/acceptance/letter/<int:letter_id>/300", updateAcceptanceLetterRequest300, name="update_acceptance_letter_request_300"),
   path("student/request/acceptance/letter/change/<int:letter_id>", requestChangeAcceptanceLetter, name="request_change_acceptance_letter"),
-  path("student/update/acceptance/letter/<int:letter_id>/200", updateAcceptanceLetterRequest200, name="update_acceptance_letter_request_200"),
-  path("student/update/acceptance/letter/<int:letter_id>/300", updateAcceptanceLetterRequest300, name="update_acceptance_letter_request_300"),
 ]
--- student/views.py
+++ student/views.py
@@ -20,13 +20,18 @@ def studentProfile(request):
   dean = FacultyDean.objects.filter(faculty=std.student_training_coordinator.dept_hod.department.faculty).last()
   hod = DepartmentHOD.objects.filter(department=std.student_training_coordinator.dept_hod.department).last()
 
+  acceptance_200 = AcceptanceLetter.objects.filter(sender_acept=std, level="200").last()
+  acceptance_300 = AcceptanceLetter.objects.filter(sender_acept=std, level="300").last()
+  
   context = {
     "std": std,
     "dean": dean,
     "hod": hod,
+    "acceptance_200": acceptance_200,
+    "acceptance_300": acceptance_300,
   }
   return render(request, "student/profile.html", context=context)
-
+  
 
 def otherViewStudentProfile(request, student_id):
   """student profile (for other when they view it, such as coordinator, training manager, etc)"""
@@ -40,7 +45,7 @@ def otherViewStudentProfile(request, student_id):
     "hod": hod,
   }
   return render(request, "student/profile_other.html", context=context)
-  
+
 
 def placementLetter(request):
   the_student_request_user = request.user
@@ -50,14 +55,13 @@ def placementLetter(request):
   hod = DepartmentHOD.objects.filter(department=std.student_training_coordinator.dept_hod.department).last()
 
   context = {
-    "None": None,
     "std": std,
     "letter": letter,
     "dean": dean,
     "hod": hod,
   }
   return render(request, "student/placement_letter.html", context=context)
-  
+
 
 def acceptanceLetter(request):
   the_student_request_user = request.user
@@ -67,66 +71,48 @@ def acceptanceLetter(request):
   hod = DepartmentHOD.objects.filter(department=std.student_training_coordinator.dept_hod.department).last()
 
   context = {
-    "None": None,
     "std": std,
     "letter": letter,
     "dean": dean,
     "hod": hod,
   }
   return render(request, "student/acceptance_letter.html", context=context)
-  
 
-def uploadedAcceptanceLetter(request):
+
+def uploadedAcceptanceLetter200(request):
+  """This view show 200 level student acceptance letter"""
   stu_usr = User.objects.get(id=request.user.id) # student user
   std = TrainingStudent.objects.filter(matrix_no=stu_usr.identification_num).first() # training student
-  coord = std.student_training_coordinator
 
-  # acceptance letter for student for 200 and 300 level
-  acceptance_200 = AcceptanceLetter.objects.filter(sender_acept=std, receiver_acept=coord, level="200").last()
-  acceptance_300 = AcceptanceLetter.objects.filter(sender_acept=std, receiver_acept=coord, level="300").last()
-
-  # acceptance letter (if coordinator approved to change) for student for 200 and 300 level
-  can_change_letter_200 = AcceptanceLetter.objects.filter(sender_acept=std, receiver_acept=coord, level="200", can_change=True).last()
-  can_change_letter_300 = AcceptanceLetter.objects.filter(sender_acept=std, receiver_acept=coord, level="300", can_change=True).last()
-
-  # filtering student declined request (is_declined=True) for 200 and 300 level
-  student_decline_req_200 = UpdateAcceptanceLetter.objects.filter(sender_acept=std, receiver_acept=coord, is_declined=True, level="200").last()
-  student_declined_req_300 = UpdateAcceptanceLetter.objects.filter(sender_acept=std, receiver_acept=coord, is_declined=True, level="300").last()
-
-  # request for changing letter
-  student_requests_300 = UpdateAcceptanceLetter.objects.filter(Q(sender_acept=std, receiver_acept=coord, is_declined=False, level="300"))
-
+  # acceptance letter for the student
+  acceptance_200 = AcceptanceLetter.objects.filter(sender_acept=std, level="200").last()
   context = {
-    "std": std,
     "acceptance_200": acceptance_200,
-    "acceptance_300": acceptance_300,
-    "student_decline_req_200": student_decline_req_200,
-    "student_declined_req_300": student_declined_req_300,
-    "can_change_letter_200": can_change_letter_200,
-    "can_change_letter_300": can_change_letter_300,
-    "student_requests_300": student_requests_300,
   }
-  return render(request, "student/upload_acceptance_letter.html", context=context)
-  
+  return render(request, "student/upload_acceptance_letter_200.html", context=context)
+
+
+def uploadedAcceptanceLetter300(request):
+  """This view show 300 level student acceptance letter"""
+  stu_usr = User.objects.get(id=request.user.id) # student user
+  std = TrainingStudent.objects.filter(matrix_no=stu_usr.identification_num).first() # training student
+
+  # acceptance letter for the student
+  acceptance_300 = AcceptanceLetter.objects.filter(sender_acept=std, level="300").last()
+  context = {
+    "acceptance_300": acceptance_300,
+  }
+  return render(request, "student/upload_acceptance_letter_300.html", context=context)
+
 
 def uploadAcceptanceLetter200(request):
-  """upload acceptance letter for 200 level student"""
+  """upload (the view that will upload) acceptance letter for 200 level student"""
   stu_usr = User.objects.get(id=request.user.id) # student user
   std = TrainingStudent.objects.filter(matrix_no=stu_usr.identification_num).first()
   coord = std.student_training_coordinator
-  acceptance_200 = AcceptanceLetter.objects.filter(sender_acept=std, receiver_acept=coord, level="200").last()
-
-  # if student.level == "200":
-  #   pass
+  
   form = UploadAcceptanceLetter(request.POST, request.FILES)
   if form.is_valid():
-    if form.cleaned_data.get("image") == None and form.cleaned_data.get("message") == "":
-      messages.success(request, f"You can't send blank space (empty)")
-    try:
-      if os.path.exists(acceptance_200.image.path):
-        os.remove(acceptance_200.image.path)
-    except:
-      pass
     instance = form.save(commit=False)
     pic_name = picture_name(instance.image.name)
     instance.image.name = pic_name
@@ -135,30 +121,17 @@ def uploadAcceptanceLetter200(request):
     instance.level = "200"
     instance.save()
     messages.success(request, f'Your 200 level acceptance letter image has been uploaded!')
-    return redirect(reverse('student:uploaded_acceptance_letter'))
-    
+    return redirect(reverse('student:uploaded_acceptance_letter_200'))
+
 
 def uploadAcceptanceLetter300(request):
-  """upload acceptance letter for 300 level student"""
+  """upload (the view that will upload) acceptance letter for 300 level student"""
   stu_usr = User.objects.get(id=request.user.id) # student user
   std = TrainingStudent.objects.filter(matrix_no=stu_usr.identification_num).first()
   coord = std.student_training_coordinator
-  acceptance_300 = AcceptanceLetter.objects.filter(sender_acept=std, receiver_acept=coord, level="300").last()
-
-  # if student.level == "300":
-  #   pass
+  
   form = UploadAcceptanceLetter(request.POST, request.FILES)
-  # , instance=request.user
   if form.is_valid():
-    if form.cleaned_data.get("image") == None and form.cleaned_data.get("message") == "":
-      messages.success(request, f"You can't send blank space (empty)")
-    try:
-      if acceptance_300:
-        acceptance_300.delete()
-      if os.path.exists(acceptance_300.image.path):
-        os.remove(acceptance_300.image.path)
-    except:
-      pass
     instance = form.save(commit=False)
     pic_name = picture_name(instance.image.name)
     instance.image.name = pic_name
@@ -167,19 +140,113 @@ def uploadAcceptanceLetter300(request):
     instance.level = "300"
     instance.save()
     messages.success(request, f'Your 300 level acceptance letter image has been uploaded!')
-    return redirect(reverse('student:uploaded_acceptance_letter'))
-    
+    return redirect(reverse('student:uploaded_acceptance_letter_300'))
 
-def requestChangeAcceptanceLetter(request, letter_id):
-  letter = AcceptanceLetter.objects.get(id=letter_id)
-  if letter.sender_acept.student == request.user:
-    context = {
-      "letter": letter,
-    }
-    return render(request, "student/request_change_acceptance_letter.html", context=context)
-  return False
+
+def updateAcceptanceLetter200(request):
+  """update acceptance letter for 200 level student"""
+  stu_usr = User.objects.get(id=request.user.id) # student user
+  std = TrainingStudent.objects.filter(matrix_no=stu_usr.identification_num).first()
+  coord = std.student_training_coordinator
+  acceptance_200 = AcceptanceLetter.objects.filter(sender_acept=std, level="200").last()
+
+  # acceptance letter (if coordinator approved to change) for the student
+  can_change_letter_200 = AcceptanceLetter.objects.filter(sender_acept=std, level="200", can_change=True).last()
+
+  # filtering student declined request (is_declined=True) for 200 level
+  student_declined_req_200 = UpdateAcceptanceLetter.objects.filter(sender_acept=std, receiver_acept=coord, is_declined=True, level="200").last()
+
+  # request for changing letter
+  student_requests_200 = UpdateAcceptanceLetter.objects.filter(Q(sender_acept=std, receiver_acept=coord, is_declined=False, level="200"))
+
+  # this will restrict user from going to update acceptance letter route,
+  # if he/she doesn't uploaded his/her acceptance letter
+  if not acceptance_200:
+    return False
   
+  if request.method == 'POST':
+    form = UploadAcceptanceLetter(request.POST, request.FILES, instance=acceptance_200)
+    if form.is_valid():
+      if os.path.exists(acceptance_200.image.path):
+        os.remove(acceptance_200.image.path)
+      form.save()
+
+      student_requests = UpdateAcceptanceLetter.objects.filter(Q(sender_acept=std, receiver_acept=coord, is_declined=False, level="200")).last()
+      if student_requests:
+        student_requests.is_declined = True
+        student_requests.save()
+      stu_letter = AcceptanceLetter.objects.filter(sender_acept=std, level="200").last()
+      if stu_letter:
+        stu_letter.is_reviewed = False
+        stu_letter.can_change = False
+        stu_letter.save()
+      messages.success(request, f'Your 200 level acceptance letter image has been updated!')
+      return redirect(reverse('student:update_acceptance_letter_200'))
+  else:
+    form = UploadAcceptanceLetter(instance=acceptance_200)
+  context = {
+    'form': form,
+    "std": std,
+    "acceptance_200": acceptance_200,
+    "student_declined_req_200": student_declined_req_200,
+    "can_change_letter_200": can_change_letter_200,
+    "student_requests_200": student_requests_200,
+  }
+  return render(request, 'student/update_acceptance_letter_200.html', context)
+
+
+def updateAcceptanceLetter300(request):
+  """update acceptance letter for 300 level student"""
+  stu_usr = User.objects.get(id=request.user.id) # student user
+  std = TrainingStudent.objects.filter(matrix_no=stu_usr.identification_num).first()
+  coord = std.student_training_coordinator
+  acceptance_300 = AcceptanceLetter.objects.filter(sender_acept=std, level="300").last()
+
+  # acceptance letter (if coordinator approved to change) for the student
+  can_change_letter_300 = AcceptanceLetter.objects.filter(sender_acept=std, level="300", can_change=True).last()
+
+  # filtering student declined request (is_declined=True) for 300 level
+  student_declined_req_300 = UpdateAcceptanceLetter.objects.filter(sender_acept=std, receiver_acept=coord, is_declined=True, level="300").last()
+
+  # request for changing letter
+  student_requests_300 = UpdateAcceptanceLetter.objects.filter(Q(sender_acept=std, receiver_acept=coord, is_declined=False, level="300"))
+
+  # this will restrict user from going to update acceptance letter route,
+  # if he/she doesn't uploaded his/her acceptance letter
+  if not acceptance_300:
+    return False
   
+  if request.method == 'POST':
+    form = UploadAcceptanceLetter(request.POST, request.FILES, instance=acceptance_300)
+    if form.is_valid():
+      if os.path.exists(acceptance_300.image.path):
+        os.remove(acceptance_300.image.path)
+      form.save()
+
+      student_requests = UpdateAcceptanceLetter.objects.filter(Q(sender_acept=std, receiver_acept=coord, is_declined=False, level="300")).last()
+      if student_requests:
+        student_requests.is_declined = True
+        student_requests.save()
+      stu_letter = AcceptanceLetter.objects.filter(sender_acept=std, level="300").last()
+      if stu_letter:
+        stu_letter.is_reviewed = False
+        stu_letter.can_change = False
+        stu_letter.save()
+      messages.success(request, f'Your 300 level acceptance letter image has been updated!')
+      return redirect(reverse('student:update_acceptance_letter_300'))
+  else:
+    form = UploadAcceptanceLetter(instance=acceptance_300)
+  context = {
+    'form': form,
+    "std": std,
+    "acceptance_300": acceptance_300,
+    "student_declined_req_300": student_declined_req_300,
+    "can_change_letter_300": can_change_letter_300,
+    "student_requests_300": student_requests_300,
+  }
+  return render(request, 'student/update_acceptance_letter_300.html', context)
+
+
 def updateAcceptanceLetterRequest200(request, letter_id):
   """Make request to change acceptance lette for 200 level"""
   stu_usr = User.objects.get(id=request.user.id) # student user
@@ -187,10 +254,10 @@ def updateAcceptanceLetterRequest200(request, letter_id):
   coord = std.student_training_coordinator
   acept_letter = AcceptanceLetter.objects.get(id=letter_id)
 
-  student_requests = UpdateAcceptanceLetter.objects.filter(Q(sender_acept=std, receiver_acept=coord, is_approved=False, level="200"))
+  student_requests = UpdateAcceptanceLetter.objects.filter(Q(sender_acept=std, receiver_acept=coord, is_declined=False, level="200"))
   if student_requests:
     messages.success(request, f'You already requested!')
-    return redirect(reverse('student:uploaded_acceptance_letter'))
+    return redirect(reverse('student:update_acceptance_letter_200'))
 
   form = UpdateAcceptanceLetterForm(request.POST)
   if form.is_valid():
@@ -203,7 +270,7 @@ def updateAcceptanceLetterRequest200(request, letter_id):
   instance.level = "200"
   instance.save()
   messages.success(request, f'Your request for updating your training acceptance letter has been sent!')
-  return redirect(reverse('student:uploaded_acceptance_letter'))
+  return redirect(reverse('student:update_acceptance_letter_200'))
 
 
 def updateAcceptanceLetterRequest300(request, letter_id):
@@ -216,7 +283,7 @@ def updateAcceptanceLetterRequest300(request, letter_id):
   student_requests = UpdateAcceptanceLetter.objects.filter(Q(sender_acept=std, receiver_acept=coord, is_declined=False, level="300"))
   if student_requests:
     messages.success(request, f'You already requested!')
-    return redirect(reverse('student:uploaded_acceptance_letter'))
+    return redirect(reverse('student:update_acceptance_letter_300'))
 
   form = UpdateAcceptanceLetterForm(request.POST)
   if form.is_valid():
@@ -229,4 +296,14 @@ def updateAcceptanceLetterRequest300(request, letter_id):
   instance.level = "300"
   instance.save()
   messages.success(request, f'Your request for updating your training acceptance letter has been sent!')
-  return redirect(reverse('student:uploaded_acceptance_letter'))
+  return redirect(reverse('student:update_acceptance_letter_300'))
+
+
+def requestChangeAcceptanceLetter(request, letter_id):
+  letter = AcceptanceLetter.objects.get(id=letter_id)
+  if letter.sender_acept.student == request.user:
+    context = {
+      "letter": letter,
+    }
+    return render(request, "student/request_change_acceptance_letter.html", context=context)
+  return False
