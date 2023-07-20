428a51e326298d2f5734d2a2f403c36f0cf4e21f
Usman Musa <usmanmusa1920@gmail.com>
2023-07-20 11:14:06 +0100

    run away from a corrupted branch

--- toolkit/center.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from faculty.models import (
-    Faculty, FacultyDean)
-from department.models import (
-    Department, DepartmentHOD, DepartmentTrainingCoordinator, StudentSupervisor, Letter)
-from administrator.models import (
-    Administrator)
-from student.models import (
-    TrainingStudent, StudentLetterRequest, AcceptanceLetter, WeekReader, WeekScannedLogbook, CommentOnLogbook)
